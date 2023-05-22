3562879ba8e19a682be4826cb753ddf813cc8647
lvan-bus <lucienvanbussel@gmail.com>
2023-05-22 13:57:46 +0200

    added prepare_arguments to main.c

--- src/main.c
+++ src/main.c
@@ -12,20 +12,34 @@
 //	return(0);
 //}
 
+static bool prepare_arguments(char **argv, t_philo *phil)
+{
+    bool	valid_number;
+
+    valid_number = true;
+    valid_number = ft_atol_with_overflow(argv[1], &phil->number_of_philosophers);
+    if (valid_number == false)
+        return (false);
+    valid_number = ft_atol_with_overflow(argv[2], &phil->time_to_die);
+    if (valid_number == false)
+        return (false);
+    valid_number = ft_atol_with_overflow(argv[3], &phil->time_to_eat);
+    if (valid_number == false)
+        return (false);
+    valid_number = ft_atol_with_overflow(argv[4], &phil->time_to_sleep);
+    if (valid_number == false)
+        return (false);
+    return (valid_number);
+}
+
 int main(int argc, char **argv)
 {
-	long	number_of_philosophers;
-	long	time_to_die;
-	bool	valid_number;
+	t_philo phil;
 
 	if (argc != 5)
 		return (0);
-	valid_number = ft_atol_with_overflow(argv[1], &number_of_philosophers);
-	if (valid_number == false)
-		return (0);
-	valid_number = ft_atol_with_overflow(argv[2], &time_to_die);
-	if (valid_number == false)
-		return (0);
-	thread_main(number_of_philosophers, time_to_die);
+    if (prepare_arguments(argv, &phil) == false)
+        return (0);
+	thread_main(&phil);
 	return (0);
 }
