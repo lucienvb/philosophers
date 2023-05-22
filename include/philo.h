168a2a5fe87d380b371921bba3842fe25abf1c89
lvan-bus <lucienvanbussel@gmail.com>
2023-05-22 13:57:06 +0200

    added a couple of variables to t_philo and added some prototypes

--- include/philo.h
+++ include/philo.h
@@ -16,13 +16,25 @@
 typedef struct s_philo
 {
 	size_t 	*arg;
-	size_t 	time_to_die;
+    long    number_of_philosophers;
+	long 	time_to_die;
+    long    time_to_eat;
+    long    time_to_sleep;
     size_t  i;
 }			t_philo;
 
 // FUNCTIONS
 bool	philo(char *str);
-int 	thread_main(size_t number_of_philosophers, size_t time_to_die);
+int 	thread_main(t_philo *phil);
 bool	ft_atol_with_overflow(const char *str, long *result);
+
+// INITIALIZE
+bool    initialize(t_philo *phil, char ***thread_result, pthread_t **thread, size_t *arg);
+
+// THREAD
+void    thread_print_result(size_t number_of_threads, char **thread_result);
+bool	thread_join(size_t number_of_threads, pthread_t *thread, char **thread_result);
+void	thread_create(size_t number_of_threads, t_philo *phil, pthread_t *thread);
+void	*thread_routine(void *tmp);
 
 #endif
