91ede25c8c12713756025f3a3f5026c5213adb90
lvan-bus <lucienvanbussel@gmail.com>
2023-05-22 13:59:35 +0200

    refactored thread_print_result

--- src/thread/thread_print_result.c
+++ src/thread/thread_print_result.c
@@ -1,3 +1,13 @@
-//
-// Created by Lucien Van Bussel on 5/22/23.
-//
+#include "include/philo.h"
+
+void thread_print_result(size_t number_of_threads, char **thread_result)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < number_of_threads)
+    {
+        printf("result thread %zu: %s\n", i + 1, thread_result[i]);
+        i++;
+    }
+}
