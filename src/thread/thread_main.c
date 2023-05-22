dd3ed51ee5a8455bc2380efc519954a2f84a71be
lvan-bus <lucienvanbussel@gmail.com>
2023-05-22 17:02:13 +0200

    some design improvements, also renamed the file to thread_main

--- src/thread/thread_main.c
+++ src/thread/thread_main.c
@@ -1,19 +1,19 @@
 //#include "philo.h"
-#include "../include/philo.h"
+#include "../../include/philo.h"
 
 int thread_main(t_philo *phil)
 {
-    char		**thread_result;
-    pthread_t	*thread;
-    size_t      *arg;
+    char			**thread_result;
+    pthread_t 		*thread;
+    size_t			*arg;
 
     thread_result = NULL;
     arg = NULL;
-    initialize(phil, &thread_result, &thread, arg);
+	initialize(phil, &thread_result, &thread, arg);
     thread_create(phil, thread);
    	// maybe free stuff when pthread_join fails (free everything that's previously malloc)
 	if (thread_join(phil->number_of_philosophers, thread, thread_result) == false)
 		return (0);
     thread_print_result(phil->number_of_philosophers, thread_result);
-	return 0;
+	return (0);
 }
