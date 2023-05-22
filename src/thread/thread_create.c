e6099f90172a5944be16d01cf871c478a9c18e1c
lvan-bus <lucienvanbussel@gmail.com>
2023-05-22 13:58:42 +0200

    refactored thread_create in a new file

--- src/thread/thread_create.c
+++ src/thread/thread_create.c
@@ -1,3 +1,21 @@
-//
-// Created by Lucien Van Bussel on 5/22/23.
-//
+#include "include/philo.h"
+
+void	thread_create(size_t number_of_threads, t_philo *phil, pthread_t *thread)
+{
+    t_philo *thread_phil;
+    size_t i;
+
+    i = 0;
+    while (i < number_of_threads)
+    {
+        thread_phil = malloc(sizeof(t_philo));
+        if (!thread_phil)
+            return ;
+        memcpy(thread_phil, phil, sizeof(t_philo));
+        // maybe free stuff when pthread_create fails (free everything that's previously malloc)
+        if (pthread_create(&thread[i], NULL, thread_routine, (void *) thread_phil) != 0)
+            return ;
+        phil->i++;
+        i++;
+    }
+}
