4b05dc3ac53d1c2a5f829e40928f6e097b573550
lvan-bus <lucienvanbussel@gmail.com>
2023-05-23 14:29:20 +0200

    updated thread_routine with new form of mutex variable

--- src/thread/thread_routine.c
+++ src/thread/thread_routine.c
@@ -6,28 +6,21 @@ void	*thread_routine(void *tmp)
     size_t			i;
     size_t			*arg;
     t_philo			*phil;
-//	pthread_mutex_t	mutex;
 
-//	pthread_mutex_init(&mutex, NULL);
     phil = (t_philo *) tmp;
 
     arg = &phil->arg[phil->i];
     i = 0;
 	printf("Philosopher %d is thinking\n", *(int *) arg);
-	if (pthread_mutex_lock(phil->mutex) == 0)
+//	if (pthread_mutex_lock(phil->mutex[phil->i]) == 0 && pthread_mutex_lock(phil->mutex[phil->i - 1]) == 0)
+	if (pthread_mutex_lock(phil->mutex[phil->i]) == 0)
 	{
 		printf("Philosopher %d is eating\n", *(int *) arg);
 		usleep(phil->time_to_eat * ten_power_six);
-		pthread_mutex_unlock(phil->mutex);
+		pthread_mutex_unlock(phil->mutex[phil->i]);
+//		pthread_mutex_unlock(phil->mutex[phil->i - 1]);
 	}
-//    while (i < (size_t) phil->time_to_die)
-//    {
-//        printf("Philosopher %d is thinking\n", *(int *) arg);
-//        usleep(250000);
-//        i++;
-//    }
     if (i == (size_t) phil->time_to_die)
         return("philosopher has died\n");
-//	pthread_mutex_destroy(&mutex);
     return ("philosopher is thinking\n");
 }
