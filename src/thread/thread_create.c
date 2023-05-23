83b5a2f086110ef93b635ca38ca04612cfd81147
lvan-bus <lucienvanbussel@gmail.com>
2023-05-23 14:27:00 +0200

    replaced memcpy by ft_memcpy

--- src/thread/thread_create.c
+++ src/thread/thread_create.c
@@ -1,6 +1,5 @@
 #include "include/philo.h"
 
-// WARNING: memcpy still in there
 void	thread_create(t_philo *phil, pthread_t *thread, pthread_mutex_t **mutex)
 {
     t_philo *thread_phil;
@@ -12,8 +11,8 @@ void	thread_create(t_philo *phil, pthread_t *thread, pthread_mutex_t **mutex)
         thread_phil = malloc(sizeof(t_philo));
         if (!thread_phil)
             return ;
-        memcpy(thread_phil, phil, sizeof(t_philo));
-		thread_phil->mutex = mutex;
+        ft_memcpy(thread_phil, phil, sizeof(t_philo));
+		thread_phil->mutex = &mutex[phil->i];
         // maybe free stuff when pthread_create fails (free everything that's previously malloc)
         if (pthread_create(&thread[i], NULL, thread_routine, (void *) thread_phil) != 0)
             return ;
