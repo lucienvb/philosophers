832df9e0509a5a361333dfc3ec6df23a79b26556
lvan-bus <lucienvanbussel@gmail.com>
2023-06-05 09:10:15 +0200

    prepare_arguments function in utils again

--- src/main.c
+++ src/main.c
@@ -1,25 +1,5 @@
 #include <philo.h>
 
-static bool prepare_arguments(char **argv, t_philo *phil)
-{
-	bool	valid_number;
-
-	valid_number = true;
-	valid_number = ft_atol_with_overflow(argv[1], &phil->number_of_philosophers);
-	if (valid_number == false)
-		return (false);
-	valid_number = ft_atol_with_overflow(argv[2], &phil->time_to_die);
-	if (valid_number == false)
-		return (false);
-	valid_number = ft_atol_with_overflow(argv[3], &phil->time_to_eat);
-	if (valid_number == false)
-		return (false);
-	valid_number = ft_atol_with_overflow(argv[4], &phil->time_to_sleep);
-	if (valid_number == false)
-		return (false);
-	return (valid_number);
-}
-
 int main(int argc, char **argv)
 {
 	t_philo	phil;
