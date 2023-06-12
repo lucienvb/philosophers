e2a39bc509669f3426457cd62daad15f4416a5e5
lvan-bus <lucienvanbussel@gmail.com>
2023-06-12 14:13:38 +0200

    update prepare arguments

--- src/utils/prepare_arguments.c
+++ src/utils/prepare_arguments.c
@@ -1,28 +1,28 @@
 #include <philo.h>
 
-bool prepare_arguments(char **argv, t_philo *phil)
+bool prepare_arguments(char **argv, t_public *data_pool)
 {
 	bool	valid_number;
 
 	valid_number = true;
-	valid_number = ft_atol_with_overflow(argv[1], &phil->number_of_philosophers);
-	phil->number_of_philosophers++;
+	valid_number = ft_atol_with_overflow(argv[1], &data_pool->number_of_philosophers);
+	data_pool->number_of_philosophers++;
 	if (valid_number == false)
 		return (false);
-	valid_number = ft_atol_with_overflow(argv[2], &phil->time_to_die);
-	if (valid_number == false || phil->time_to_die < 1)
+	valid_number = ft_atol_with_overflow(argv[2], &data_pool->time_to_die);
+	if (valid_number == false || data_pool->time_to_die < 1)
 		return (false);
-	valid_number = ft_atol_with_overflow(argv[3], &phil->time_to_eat);
+	valid_number = ft_atol_with_overflow(argv[3], &data_pool->time_to_eat);
 	if (valid_number == false)
 		return (false);
-	valid_number = ft_atol_with_overflow(argv[4], &phil->time_to_sleep);
+	valid_number = ft_atol_with_overflow(argv[4], &data_pool->time_to_sleep);
 	if (valid_number == false)
 		return (false);
 
 	// printing the parameters
-	printf("\nnumber of philosophers: %zu\n", phil->number_of_philosophers);
-	printf("time to die: %zu\n", phil->time_to_die);
-	printf("time to eat: %zu\n", phil->time_to_eat);
-	printf("time to sleep: %zu\n\n", phil->time_to_sleep);
+	printf("\nnumber of philosophers: %zu\n", data_pool->number_of_philosophers);
+	printf("time to die: %zu\n", data_pool->time_to_die);
+	printf("time to eat: %zu\n", data_pool->time_to_eat);
+	printf("time to sleep: %zu\n\n", data_pool->time_to_sleep);
 	return (valid_number);
 }
