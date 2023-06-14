a2e30e41b6fb50f497766bd97544aaf6a8527ba2
lvan-bus <lucienvanbussel@gmail.com>
2023-06-14 12:05:01 +0200

    added print function and implemented the pointer mutex with enum

--- src/thread/thread_new.c
+++ src/thread/thread_new.c
@@ -23,12 +23,20 @@ static void	start_sign_parent(pthread_mutex_t *start_mutex, int num_threads, int
 	pthread_mutex_unlock(start_mutex);
 }
 
+static void	print(pthread_mutex_t *print, long id, char *message)
+{
+	pthread_mutex_lock(print);
+	printf("%ld", id);
+	printf(" %s\n", message);
+	pthread_mutex_unlock(print);
+}
+
 static void	start_sign_threads(t_philo *phil)
 {
 	pthread_mutex_t *start_mutex;
 
-	start_mutex = &phil->data_pool->start_mutex;
-	printf("Thread %ld starting operation\n", (long)phil->id);
+	start_mutex = &phil->data_pool->mutex[0];
+	print(&phil->data_pool->mutex[1], (long)phil->id, "starts");
 	pthread_mutex_lock(start_mutex);
 	phil->data_pool->thread_counter++;
 	pthread_mutex_unlock(start_mutex);
@@ -48,11 +56,33 @@ static void	start_sign_threads(t_philo *phil)
 static void	*thread_function(void* arg)
 {
 	t_philo	        *phil;
+	pthread_mutex_t	*stop;
 
 	phil = (t_philo *)arg;
-
 	start_sign_threads(phil);
-	printf("Thread %ld finished operation\n", (long)phil->id);
+	stop = &phil->data_pool->mutex[2];
+	while (1)
+	{
+		usleep(1);
+		print(&phil->data_pool->mutex[1], (long)phil->id, "inside while loop");
+//		printf("thread %ld in while loop\n", (long)phil->id);
+		pthread_mutex_lock(stop);
+		if (phil->data_pool->dead == true)
+		{
+			pthread_mutex_unlock(stop);
+			break ;
+		}
+		if (phil->id == 1)
+		{
+			phil->data_pool->dead = true;
+			printf("%ld equals 1\n", (long)phil->id);
+			pthread_mutex_unlock(stop);
+			break ;
+		}
+
+		pthread_mutex_unlock(stop);
+	}
+	print(&phil->data_pool->mutex[1], (long)phil->id, "stops");
 	return (NULL);
 }
 
@@ -66,9 +96,10 @@ int thread_main_new(t_public *data_pool)
 	philos = malloc(data_pool->number_of_philosophers * sizeof(t_philo));
 	if (!philos)
 		return (0);
-	if (pthread_mutex_init(&data_pool->start_mutex, NULL) != 0)
-		return (free(philos), 0);
-    start_mutex = &data_pool->start_mutex;
+	if (mutex_initialize(&data_pool->mutex, 3) == false)
+		  return (free(philos), 0);
+	data_pool->dead = false;
+	start_mutex = &data_pool->mutex[0];
 	threads = malloc(data_pool->number_of_philosophers * sizeof(pthread_t));
 	if (!threads)
 		return (free(philos), pthread_mutex_destroy(start_mutex), 0);
@@ -91,6 +122,6 @@ int thread_main_new(t_public *data_pool)
 		phil.id++;
 	}
 	free(threads);
-	pthread_mutex_destroy(start_mutex);
+	pthread_mutex_destroy(data_pool->mutex);
 	return (0);
 }
