7224a395ddce853fa8379c2c0cbdb5f9e814b616
lvan-bus <lucienvanbussel@gmail.com>
2023-06-19 17:37:15 +0200

    thread_function now thread_routine again and in the thread_routine file

--- src/thread/thread_routine.c
+++ src/thread/thread_routine.c
@@ -1,78 +1,36 @@
 #include <philo.h>
 
-
-void	*thread_routine(void *tmp)
+void	*thread_routine(void* arg)
 {
-    size_t *id;
-    t_philo *phil;
+	pthread_mutex_t	*stop;
+	t_philo	        *phil;
+	t_time          time;
+	bool            first_meal;
 
-	phil = (t_philo *) tmp;
-	id = &phil->id[phil->i];
-	printf("Philosopher %d starts\n", *(int *) id);
-	pthread_mutex_lock(&phil->data_pool->start_mutex);
-	pthread_mutex_unlock(&phil->data_pool->start_mutex);
-
-	pthread_mutex_lock(&phil->data_pool->mutex[2]);
-	phil->data_pool->test += 10;
-	printf("Philosopher %d, test: %zu\n\n", *(int *) id, phil->data_pool->test);
-    pthread_mutex_unlock(&phil->data_pool->mutex[2]);
-
-    return (NULL);
-//	printf("Philosopher %d ends\n", *(int *) id);
-}
-
-//    pthread_mutex_lock(&phil->mutex[1]);
-//    phil->test -= 2;
-//    printf("Philosopher %d, test: %zu\n\n", *(int *) id, phil->test);
-//    pthread_mutex_unlock(&phil->mutex[1]);
-
-// Need to fix that all the threads use the same mutexes and not copies of the same mutexes like now
-/*void	*thread_routine(void *tmp)
-{
-    size_t	i;
-    size_t	*id;
-    t_philo	*phil;
-	int32_t	previous_mutex_index;
-
-    phil = (t_philo *) tmp;
-	previous_mutex_index = (int32_t) phil->i - 1;
-	if (previous_mutex_index == -1)
-		previous_mutex_index = phil->number_of_philosophers - 1;
-    id = &phil->id[phil->i];
-//	printf("---------------------------------\n");
-//	printf("Philosopher %d:\n", *(int *) id);
-//	printf("current_mutex_index: %zu\n", phil->i);
-//	printf("previous_mutex_index: %i\n", previous_mutex_index);
-//	printf("number of philosophers: %zu\n", phil->number_of_philosophers);
-//	printf("time to die: %zu\n", phil->time_to_die);
-//	printf("time to eat: %zu\n", phil->time_to_eat);
-//	printf("time to sleep: %zu\n", phil->time_to_sleep);
-//	printf("---------------------------------\n\n");
-	i = 0;
-	if (phil->number_of_philosophers == 1)
+	phil = (t_philo *)arg;
+	initialize_time(&time, phil);
+	pthread_mutex_lock(&phil->data_pool->mutex[START]);
+	pthread_mutex_unlock(&phil->data_pool->mutex[START]);
+	stop = &phil->data_pool->mutex[STOP];
+	first_meal = true;
+	while (1)
 	{
-		usleep(phil->time_to_die * THOUSAND);
-		return ("Philosopher has died\n");
-	}
-	while (i < (size_t) phil->time_to_die)
-	{
-		i++;
-		if ((pthread_mutex_lock(&phil->mutex[phil->i]) == 0 && pthread_mutex_lock(&phil->mutex[previous_mutex_index])) == 0)
+		// check if it needs to stop
+		if (first_meal == true)
 		{
-			printf("Philosopher %d is eating\n", *(int *) id);
-			usleep(phil->time_to_eat * THOUSAND);
-			if (pthread_mutex_unlock(&phil->mutex[phil->i]) != 0)
-				return ("unlocking mutex failed\n");
-			if (pthread_mutex_unlock(&phil->mutex[previous_mutex_index]) != 0)
-				return ("unlocking mutex failed\n");
-			i = 0;
-//			printf("Philosopher %d is sleeping\n", *(int *) id);
-//			usleep(phil->time_to_sleep * THOUSAND);
-//			printf("Philosopher %d is thinking\n", *(int *) id);
+			time.time_since_last_meal = time_of_day_ms();
+			first_meal = false;
 		}
-		break ;
+		if (time_sleep_and_validate(phil->data_pool->time_to_eat, phil) == false)
+			break ;
+		if (diner(phil, &time) == false)
+			break ;
+		time.time_since_last_meal = time_of_day_ms();
+		if (sleeping(phil, &time) == false)
+			break ;
+		print(&phil->data_pool->mutex[PRINT], (long)phil->id, "is thinking", time.start);
 	}
-    free(phil);
-//	printf("Philosopher %d has died\n", *(int *) id);
-    return ("Philosopher has died\n");
-}*/
+	if (phil->dead == true)
+		print(&phil->data_pool->mutex[PRINT], (long)phil->id, "died", time.start);
+	return (NULL);
+}
