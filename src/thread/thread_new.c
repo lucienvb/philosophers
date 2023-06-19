89e67f54bd82181f74afd57df752acf89e8c2946
lvan-bus <lucienvanbussel@gmail.com>
2023-06-19 15:11:01 +0200

    reached_meal_count checker implemented in check_if_alive, possibly have to move later

--- src/thread/thread_new.c
+++ src/thread/thread_new.c
@@ -18,6 +18,8 @@ static void	print(pthread_mutex_t *print, long id, char *message, int64_t start)
 
 static bool	check_if_alive(t_philo *phil, int64_t time_since_last_meal, long time_to_die)
 {
+	long	number_of_philosophers;
+
 	pthread_mutex_lock(&phil->data_pool->mutex[TIME]);
 	if (time_difference_ms(time_of_day_ms(), time_since_last_meal) >= (int64_t) time_to_die)
 	{
@@ -27,6 +29,15 @@ static bool	check_if_alive(t_philo *phil, int64_t time_since_last_meal, long tim
 		return (false);
 	}
 	pthread_mutex_unlock(&phil->data_pool->mutex[TIME]);
+
+	ft_memcpy(&number_of_philosophers, &phil->data_pool->number_of_philosophers, sizeof(long));
+	pthread_mutex_lock(&phil->data_pool->mutex[MEALS]);
+	if (phil->data_pool->reached_meal_count == number_of_philosophers)
+	{
+		pthread_mutex_unlock(&phil->data_pool->mutex[MEALS]);
+		phil->data_pool->stop = true;
+	}
+	pthread_mutex_unlock(&phil->data_pool->mutex[MEALS]);
 	return (true);
 }
 
