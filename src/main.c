c590c3578fc594718258dd5333ec07e6f856ec12
lvan-bus <lucienvanbussel@gmail.com>
2023-06-19 15:09:23 +0200

    handled case of one philosopher in the main

--- src/main.c
+++ src/main.c
@@ -7,9 +7,15 @@ int main(int argc, char **argv)
     (void)argv;
 	if (parser(argc) == false)
 		return (0);
-    if (prepare_arguments(argv, &data_pool) == false)
+    if (prepare_arguments(argc, argv, &data_pool) == false)
 	{
 		printf("invalid input\n");
+		return (0);
+	}
+	if (data_pool.number_of_philosophers == 1)
+	{
+		time_sleep_ms(data_pool.time_to_die);
+		printf("%ld 1 died\n", (long) data_pool.time_to_die);
 		return (0);
 	}
 	thread_main_new(&data_pool);
